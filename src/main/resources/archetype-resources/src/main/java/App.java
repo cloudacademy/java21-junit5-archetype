57294eef1dd673764bce8b716d34530eef2f3f93
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-07-10 09:56:05 +1200

    updates

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,10 @@
+package ${package};
+
+public class App {
+
+    public static void main(String[] args) {
+        var sum = Calculator.Add(40, 2);
+        System.out.println("CloudAcademy DevOps 2020!! Answer: " + sum);
+    }
+
+}
